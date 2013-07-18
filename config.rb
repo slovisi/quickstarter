6982eb2386e1cda12099ececbcecd6ab439ac925
Sylvain Lovisi <sylvain.lovisi@gmail.com>
2013-07-18 18:07:54 +0200

    Minor tweeking

--- config.rb
+++ config.rb
@@ -9,7 +9,7 @@ images_dir = "img"
 javascripts_dir = "js"
 # Uncomment the following line if you use webfonts, 
 # change the value to reflect your own structure
-# fonts_dir = "font"
+fonts_dir = "font"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
